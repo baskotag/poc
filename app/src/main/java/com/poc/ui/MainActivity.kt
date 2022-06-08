65578c0370cf177ad3a0b2177e7703e40ca811f2
baskotag <basavaraj.kotagond@publicissapient.com>
2022-06-08 11:43:34 +0530

    - App Theme Added - App Bar Added

--- app/src/main/java/com/poc/ui/MainActivity.kt
+++ app/src/main/java/com/poc/ui/MainActivity.kt
@@ -1,10 +1,52 @@
 package com.poc.ui
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.poc.R
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.poc.databinding.ActivityMainBinding
+import com.poc.utility.hide
+import com.poc.utility.show
+import com.poc.viewModel.ActivityViewModel
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.activityViewModel = ActivityViewModel()
+        setContentView(binding.rootView)
+        setSupportActionBar(binding.toolbar)
+
+        setupNavigation()
+
+        //binding.toolbar.se
     }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.containerFragment)
+        setupActionBarWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashFragment -> {
+                    binding.appBarLayout.hide()
+                }
+                R.id.homeFragment -> {
+                    setupToolbarTitleAndAction(getString(R.string.home), false)
+                }
+                R.id.detailsFragment -> {
+                    setupToolbarTitleAndAction(getString(R.string.details), true)
+                }
+            }
+        }
+    }
+
+    private fun setupToolbarTitleAndAction(title: String, isDisplayHomeAsUp: Boolean) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(isDisplayHomeAsUp)
+        binding.activityViewModel?.toolbarTitle?.set(title)
+        binding.appBarLayout.show()
+    }
+
 }
---
+++ app/src/main/java/com/poc/viewModel/ActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.poc.viewModel
+
+import androidx.databinding.ObservableField
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+class ActivityViewModel  @Inject internal constructor() : ViewModel() {
+    var toolbarTitle: ObservableField<String> = ObservableField()
+}
