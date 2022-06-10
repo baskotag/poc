25c2db1d26409bcc31a61025dccfe4a00f41853e
baskotag <basavaraj.kotagond@publicissapient.com>
2022-06-10 18:22:47 +0530

    - Added Test Caces

--- app/src/androidTest/java/com/poc/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/poc/ExampleInstrumentedTest.kt
@@ -1,5 +1,10 @@
 package com.poc
 
+import android.R
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
---
+++ app/src/androidTest/java/com/poc/ui/MovieListFeature.kt
@@ -0,0 +1,29 @@
+package com.poc.ui
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.poc.R
+import kotlinx.coroutines.delay
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MovieListFeature {
+    val mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+        @Rule get
+
+    @Test
+    suspend fun displayScreenTitle() {
+        onView(withId(R.id.rootView))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        delay(3000)
+        onView(withId(R.id.toolbar))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.toolbar))
+            .check(matches(withText(R.string.home)))
+    }
+}
